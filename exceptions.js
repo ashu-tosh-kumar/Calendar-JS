21df155808828f725885fa9f30d381cc4ba2ad84
Ashutosh <37182127+ashu-tosh-kumar@users.noreply.github.com>
2022-02-28 17:43:52 +0530

    Add exceptions.js

---
+++ exceptions.js
@@ -0,0 +1,11 @@
+class InvalidDateFormat extends Error {
+    // Raised if user passes the date in invalid format
+    constructor(message) {
+        super(message);
+        this.name = "InvalidDateFormat";
+    }
+}
+
+export {
+    InvalidDateFormat
+}
