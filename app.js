f3e2fae47ecb8d411d64afc7a8c3c8b96af85f98
Ashutosh <37182127+ashu-tosh-kumar@users.noreply.github.com>
2022-03-01 13:19:24 +0530

    Fix imports; Send error message for bad requests; Fix API REST end point to get date matrix

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-import { InvalidDateFormat } from "exceptions";
 import express from "express";
-import { getDateMatrix } from "model";
+import { InvalidDateFormat } from "./exceptions.js";
+import { getDateMatrix } from "./model.js";
 
 const app = express();
 const port = 8080;
@@ -9,7 +9,8 @@ app.get('/', (req, res) => {
     res.send("Welcome to Calendar App. Please visit url: 'hostname:port/date' to try it.");
 })
 
-app.get('/', (date, res) => {
+app.get('/:date', (req, res) => {
+    const date = req.params.date;
     console.info(`GET call received to get date for: ${date}`);
     try {
         const dateMatrix = getDateMatrix(date);
@@ -20,7 +21,7 @@ app.get('/', (date, res) => {
         if (error instanceof InvalidDateFormat) {
             console.info("Date validation failed");
             res.status(400);
-            return res.send(error);
+            return res.send(error.message);
         } else {
             console.error("Server side error. Please reach out to support team for help");
             res.status(500);
